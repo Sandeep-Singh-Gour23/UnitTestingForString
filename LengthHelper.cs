07bbca15ef68288f31ce1c4aa8f9eb94b69469ad
Sandeep-Singh-Gour23 <sandeepsinghgour0@gmail.com>
2022-06-28 17:37:11 +0530

    Add project files.

---
+++ LengthHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UnitTestingForString
+{
+    public class LengthHelper
+    {
+        public static int findLength(string str)
+        {
+            int l = 0;
+            foreach (char c in str)
+            {
+                l += 1;
+
+            }
+            return l;
+        }
+        static void Main(string[] args)
+        {
+            string str;
+            int len = 0;
+            Console.Write("Enter Your String:");
+            str = Console.ReadLine();
+            len = findLength(str);
+            Console.Write("Length of the string is : {0}\n\n", len);
+
+        }
+    }
+}
